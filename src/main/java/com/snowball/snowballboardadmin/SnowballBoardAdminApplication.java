224cba531c2e128d2a49fee7fee77b848ef434d4
matrixpower1004 <matrixpower1004@gmail.com>
2023-07-12 16:00:17 +0900

    Initialize: project setup
    * 팀원 회의에서 프로젝트를 분리하기로 결정.
    * 프로젝트 분리를 위한 의존성 설정
    * Java 17, Spring boot 2.7.13, Web, MyBatis, Security, Validation, Mail, DevTools, Thymeleaf, Lombok

---
+++ src/main/java/com/snowball/snowballboardadmin/SnowballBoardAdminApplication.java
@@ -0,0 +1,13 @@
+package com.snowball.snowballboardadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowballBoardAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnowballBoardAdminApplication.class, args);
+    }
+
+}
