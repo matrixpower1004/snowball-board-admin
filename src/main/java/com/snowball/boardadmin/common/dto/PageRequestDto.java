1a91ebfa976eb02b1d5035ddd118b60af261985b
matrixpower1004 <matrixpower1004@gmail.com>
2023-07-19 09:21:47 +0900

    Feat #11 - paging
    * 게시판 페이징 보기를 위한 코드 작성
    * dto 변경에 따른 mybatis xml 수정
    * 페이징 기본 테스트 작성

---
+++ src/main/java/com/snowball/boardadmin/common/dto/PageRequestDto.java
@@ -0,0 +1,29 @@
+package com.snowball.boardadmin.common.dto;
+
+import com.snowball.boardadmin.common.pagination.Pageable;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * author         : Jason Lee
+ * date           : 2023-07-19
+ * description    :
+ */
+@Getter
+public class PageRequestDto {
+    private final int page;
+    private final int size;
+    private final String option;
+    private final String keyword;
+
+    public PageRequestDto(Pageable pageable, SearchDto searchDto) {
+        this.size= pageable.getSize();
+        this.page= pageable.getPage();
+        this.option= searchDto.option();
+        this.keyword= searchDto.keyword();
+    }
+
+    public int getOffset() {
+        return (getPage() - 1) * getSize();
+    }
+}
---
+++ src/main/java/com/snowball/boardadmin/common/pagination/PageImpl.java
@@ -0,0 +1,57 @@
+package com.snowball.boardadmin.common.pagination;
+
+import org.springframework.util.Assert;
+
+/**
+ * author         : Jason Lee
+ * date           : 2023-07-18
+ * description    :
+ */
+public record PageImpl(Pageable pageable, int totalRecord) {
+
+    public static final int NAV_SIZE = 10; // 화면 하단에 보여쥴 navigation bar size
+
+    public PageImpl {
+
+        Assert.notNull(pageable, "pageable muusts not be null!");
+        Assert.notNull(totalRecord, "totalRecord must not be null!");
+
+    }
+
+    // 현재 페이지가 totalPage보다 크지 않게
+    public int getPageNumber() {
+        return (int) Math.min(pageable.getPage(), getTotalPage());
+    }
+
+    public long getTotalPage() {
+        return totalRecord / pageable.getSize() +
+                (totalRecord % pageable.getSize() == 0 ? 0 : 1);
+    }
+
+    public int getBeginPage() {
+        return (pageable.getPage() - 1) / NAV_SIZE * NAV_SIZE + 1;
+    }
+
+    public int getEndPage() {
+        return (int) Math.min(getBeginPage() + NAV_SIZE - 1, getTotalPage());
+    }
+
+    public boolean showPrev() {
+        return getBeginPage() != 1;
+    }
+
+    public boolean showNext() {
+        return getEndPage() != getTotalPage();
+    }
+
+    public void print() {
+        System.out.println("page=" + pageable.getPage());
+        System.out.print(showPrev() ? "PREV " : "");
+
+        for (int i = getBeginPage(); i <= getEndPage(); i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println(showNext() ? " NEXT" : "");
+    }
+
+}
